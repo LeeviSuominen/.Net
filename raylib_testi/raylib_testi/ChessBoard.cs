bfa18834782e55e1b63828fde7bf334539d2cf43
LeeviSuominen <115700342+LeeviSuominen@users.noreply.github.com>
2023-04-04 14:11:04 +0300

    Added Raylib

---
+++ raylib_testi/raylib_testi/ChessBoard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_CsLo;
+
+namespace raylib_testi
+{
+    internal class ChessBoard
+    {
+        public ChessBoard() 
+        {
+
+        }
+
+        public void Draw(int size, int start_x, int start_y)
+        {
+            Raylib.ClearBackground(Raylib.DARKGRAY);
+            string letters = "abcdefgh";
+
+            int columns = 8;
+            //int size = 40;
+            int rows = 8;
+
+            Color[] colors = new Color[] {Raylib.BROWN, Raylib.WHITE};
+
+            //int start_x = 30;
+            //int start_y = 30;
+            int colorOffset = 0;
+            for (int r = 0; r < rows; r++)
+            {
+                int y = start_y + r * size;
+                Raylib.DrawText((1 + r).ToString(), 12, y + size / 2, 20, Raylib.BLACK);
+                for (int c = 0; c < columns; c++)
+                {
+                    int x = start_x + c * size;
+                    if(r == 0)
+                    {
+                        Raylib.DrawText(letters[c].ToString(), x + size/2, start_y - size / 2, 20, Raylib.BLACK);
+                    }
+                    Raylib.DrawRectangle(x, y, size, size, colors[(c + r) % 2]);
+                    Raylib.DrawRectangleLines(x, y, size, size, Raylib.BLACK);
+                }
+                colorOffset++;
+            }
+        }
+    }
+}
---
+++ raylib_testi/raylib_testi/Program.cs
@@ -0,0 +1,55 @@
+﻿using Raylib_CsLo;
+using raylib_testi;
+using System.Drawing;
+
+namespace raylib_pohja
+{
+    class Program
+    {
+        
+        public static void Main()
+        {
+            WindowTest test = new WindowTest();
+            test.Run();
+        }
+
+    }
+
+    class WindowTest
+    {
+        const int screen_width = 720;
+        const int screen_height = 430;
+        public WindowTest()
+        {
+
+        }
+
+        public void Run()
+        {
+            Raylib.InitWindow(screen_width, screen_height, "Raylib");
+            Raylib.SetTargetFPS(60);
+
+            while (Raylib.WindowShouldClose() == false)
+            {
+                Update();
+                Draw();
+            }
+
+            Raylib.CloseWindow();
+        }
+
+        private void Draw()
+        {
+            ChessBoard board = new ChessBoard();
+            Raylib.BeginDrawing();
+            board.Draw(40, 30, 30);
+            Raylib.EndDrawing();
+        }
+
+        private void Update()
+        {
+            // Update game here
+        }
+    }
+        
+}
