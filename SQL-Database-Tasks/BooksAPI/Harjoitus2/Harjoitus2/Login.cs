3761be417122a26980450b09870f1efda0096220
LeeviS <115700342+LeeviSuominen@users.noreply.github.com>
2023-09-07 11:46:38 +0300

    My first database task.

---
+++ SQL-Database-Tasks/BooksAPI/Harjoitus2/Harjoitus2/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus2
+{
+    public class Login
+    {
+        [Key]
+        public string? Pincode { get; set; }
+        public string? Nickname { get; set; }
+
+    }
+}
